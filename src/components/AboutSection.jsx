970ac2ef656f44170c7465dd66f03a1ea2fed219
Akash Gaur <akashgaur8805@gmail.com>
2025-05-06 01:54:56 +0530

    form functionality

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -35,7 +35,11 @@ export const AboutSection = () => {
                             <a href="#contact" className="cosmic-button">
                                 Get In Touch
                             </a>
-                            <a href="" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
+                            <a 
+                            href="https://drive.google.com/uc?export=download&id=1N8XybE0tEnAotZFrB9bIcL5Fw9WNU6i_"
+                            target="_blank"
+                            rel="noopener noreferrer" 
+                            className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
                                 Download Resume
                             </a>
 
